d214aa576621942304606dd19267d34d3a0bfd07
Ravindra Bhati <ritikbhati002@gmail.com>
2022-10-12 22:59:42 +0530

    2darray

---
+++ Array2D.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[2][2];
+for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+        cin >> arr[i][j];
+    
+    }
+}
+for(int i=0;i<2;i++){
+     for(int j=0;j<2;j++){
+
+    cout << arr[i][j]<< " ";
+    }
+    cout <<endl;
+}
+}
