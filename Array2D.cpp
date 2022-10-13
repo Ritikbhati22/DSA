d51a1f8df8bd7b3530101b3f2e4b0acf0c8ccf98
Ravindra Bhati <ritikbhati002@gmail.com>
2022-10-13 23:46:50 +0530

    linearh

--- Array2D.cpp
+++ Array2D.cpp
@@ -1,34 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-bool tochecktarget(int arr[][3],int target,int row,int col){
-    {
-for(int i =0;i<2;i++){
-    for(int j=0;j<2;j++){
-        if(target == arr[i][j]){
-            return 1;
+bool linearSearch(int arr[][2], int target, int row, int col)
+{
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            if(arr[row][col]  == target){
+                return 1;
+            }
         }
-        return 0;
     }
+    return 0;
 }
-    }
+int main()
+{
+    int arr[2][2];
+    for (int i = 0; i < 2; i++)
+    {
 
-}
-int main(){
-int arr[2][2];
-for(int i=0;i<2;i++){
-    for(int j=0;j<2;j++){
-        cin >> arr[i][j];
-    
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> arr[i][j];
+        }
     }
-}
-for(int i=0;i<2;i++){
-     for(int j=0;j<2;j++){
-
-    cout << arr[i][j]<< " ";
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-
-    cout <<endl;
     int target;
+    cout << "enter the target";
     cin >> target;
-    if(tochecktarget(arr,target,i,j);
+    if (linearSearch(arr, target, 2, 2))
+    {
+        cout << "element is found";
+    }
+    else
+    {
+        cout << "not found";
+    }
+    return 0;
 }
